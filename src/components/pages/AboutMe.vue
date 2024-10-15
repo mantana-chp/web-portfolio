97990cb0e6aa0d7fddb2e6b25c4a8dd14970fd81
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-10-15 21:08:42 +0700

    about me info

---
+++ src/components/pages/AboutMe.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <div
+    className="AboutMe w-full h-screen flex justify-center items-center bg-[#FFFEF3] font-poppins"
+  >
+    <div className="flex justify-between">
+      <div className="text-4xl">About Me</div>
+      <div className="flex flex-col gap-8 text-xl">
+        <h2>Fullname: Mantana Kodcharin</h2>
+        <h2>Nickname: Chompooh</h2>
+        <h2>Date of Birth: 13 July 2003</h2>
+        <h2>Age: 21 years</h2>
+        <h2>Hobbies: Listening Music, Drawing</h2>
+        <div className="education flex flex-col gap-2">
+          <div>
+            <h2>Education</h2>
+          </div>
+          <div className="high-school text-sm">
+            <h3>2015 - 2021</h3>
+            <h3>Science - Math</h3>
+            <h3>Rayongwittayakom School</h3>
+          </div>
+          <div className="uni text-sm pt-4">
+            <h3>2022 - Present</h3>
+            <h3>Science - Math</h3>
+            <h3>Rayongwittayakom School</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
