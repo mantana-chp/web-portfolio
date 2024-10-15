a5a322307badf34d1e9d2c33e6e9fc525a77948d
mantana.chpp <110712956+mantana-chp@users.noreply.github.com>
2024-10-15 20:40:46 +0700

    starter proj

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from '@/components/partials/Navbar.vue'
+</script>
+
+<template>
+  <Navbar />
+</template>
+
+<style scoped></style>
---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="w-full">
+    <div>MANTANA WEBFLIO</div>
+  </div>
+</template>
+
+<style scoped></style>
